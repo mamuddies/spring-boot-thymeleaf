156f98acbb19f50c65d06e76c2071649c9425fa2
mamuddies <mamuddies@gmail.com>
2017-07-18 19:33:37 +0530

    Update TimeCompare.java

--- src/main/java/com/example/springbootthymeleafmmetrics/TimeCompare.java
+++ src/main/java/com/example/springbootthymeleafmmetrics/TimeCompare.java
@@ -11,10 +11,12 @@ public class TimeCompare {
     public static void main(String[] args) throws ParseException {
         // Cache map start
         String stringDate = "08/2017";
-        Date inputDate = new SimpleDateFormat("MM/yyyy").parse(stringDate);
+        List<String> listDate = new ArrayList<String>();
+        listDate.add(stringDate);
+        // Date inputDate = new SimpleDateFormat("MM/yyyy").parse(stringDate);
         Map<String, Map<String, Object>> cacheParentMap = new HashMap<String, Map<String, Object>>();
         Map<String, Object> cacheChaildMap = new HashMap<String, Object>();
-        cacheChaildMap.put("disabilly-Date", inputDate);
+        cacheChaildMap.put("disabilly-Date", listDate);
         cacheParentMap.put("disabilly", cacheChaildMap);
         // Cache map End
 
@@ -25,16 +27,19 @@ public class TimeCompare {
         Map<String, Map<String, Object>> newParentMap = new HashMap<String, Map<String, Object>>(cacheParentMap);
 
         if (newParentMap.get("disabilly") != null && newParentMap.get("disabilly").get("disabilly-Date") != null) {
-            Date d1 = (Date) newParentMap.get("disabilly").get("disabilly-Date");
+            List<String> dateList = (List<String>) newParentMap.get("disabilly").get("disabilly-Date");
+            System.out.println(dateList.get(0));
+            Date d1 = new SimpleDateFormat("MM/yyyy").parse(dateList.get(0));
             if (d2.compareTo(d1) <= 0) {
                 System.out.println("Course start aug 18 or later");
             } else {
-                System.out.println("Continue... Your logic here");
-                // write your remaining logic here
+                System.out.println("Continue... Your logic here"); // write your remaining logic here }
+
             }
 
         }
-
     }
+
+
 
 }

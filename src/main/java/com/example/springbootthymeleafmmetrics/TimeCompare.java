82ac1022fbec959a75f4330cf071c3e3261e3607
mamuddies <mamuddies@gmail.com>
2017-07-21 16:46:50 +0530

    Update TimeCompare.java

--- src/main/java/com/example/springbootthymeleafmmetrics/TimeCompare.java
+++ src/main/java/com/example/springbootthymeleafmmetrics/TimeCompare.java
@@ -1,51 +1,20 @@
-package sample.metrics.dropwizard;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-public class TimeCompare {
-
-    public static void main(String[] args) throws ParseException {
-        // Cache map start
-        String stringArrayDate[] = {"08", "2018"};
-        Map<String, Map<String, Object>> cacheParentMap = new HashMap<String, Map<String, Object>>();
-        Map<String, Object> cacheChaildMap = new HashMap<String, Object>();
-        cacheChaildMap.put("course.start.details", stringArrayDate);
-        cacheParentMap.put("course-start-date-details", cacheChaildMap);
-        // Cache map End
-
-        // Your code will start from here
-        String StringDate = "08/2018";
-        Date d2 = new SimpleDateFormat("MM/yyyy").parse(StringDate);
-
-        StringBuffer strDate = new StringBuffer();
-        Map<String, Map<String, Object>> newParentMap = new HashMap<String, Map<String, Object>>(cacheParentMap);
-
-        if (newParentMap.get("course-start-date-details") != null
-                && newParentMap.get("course-start-date-details").get("course.start.details") != null) {
-            String dateArray[] = (String[]) newParentMap.get("course-start-date-details").get("course.start.details");
-
-            for (int i = 0; i <= dateArray.length - 1; i++) {
-                strDate = strDate.append(dateArray[i]);
-                if (i != dateArray.length - 1) {
-                    strDate.append("/");
-
+.isVisable(()->{
+            String[] answer=null;
+            int month,year=0;
+            Map<String,Object> isAnswaredPreviousQuestion=null;
+            Map<String,Map<String,Object>> applicationAnswar=null;
+            if(null!=this.applicationAnswar.getAnswers()){
+                applicationAnswar=this.applicationAnswar.getAnswers();
+                isAnswaredPreviousQuestion =applicationAnswar.get("course-start-date-details");
+                if(null!=isAnswaredPreviousQuestion){
+                    answer=(String[])isAnswaredPreviousQuestion.get("coursr.start.details");
+                    if(null!=answer){
+                        month=Integer.parseInt(answer[0]);
+                        year=Integer.parseInt(answer[1]);
+                        
+                        Page page =pageService.gatPageForm("sample-exit");
+                        page.getConditions.get(0).setVisibilityConfig(year<2018||(year==2018 && month<8));
+                    }
                 }
             }
-            Date d1 = new SimpleDateFormat("MM/yyyy").parse(strDate.toString());
-            if (d1.compareTo(d2) >= 0) {
-                System.out.println("Course start aug 18 or later");
-                return;// It will come out from this method
-            } else {
-                // write your remaining logic here
-                System.out.println("Continue... Your logic here");
-
-            }
-
-        }
-    }
-
-}
+        }).doneCondition().build();

593fe55901ee32013b895ee7242328a294ad5476
mamuddies <mamuddies@gmail.com>
2017-07-23 19:52:42 +0530

    Update TimeCompare.java

--- src/main/java/com/example/springbootthymeleafmmetrics/TimeCompare.java
+++ src/main/java/com/example/springbootthymeleafmmetrics/TimeCompare.java
@@ -1,20 +1,40 @@
-.isVisable(()->{
-            String[] answer=null;
-            int month,year=0;
-            Map<String,Object> isAnswaredPreviousQuestion=null;
-            Map<String,Map<String,Object>> applicationAnswer=null;
-            if(null!=this.applicationAnswer.getAnswers()){
-                applicationAnswer=this.applicationAnswer.getAnswers();
-                isAnswaredPreviousQuestion =applicationAnswer.get("course-start-date-details");
-                if(null!=isAnswaredPreviousQuestion){
-                    answer=(String[])isAnswaredPreviousQuestion.get("course.start.details");
-                    if(null!=answer){
-                        month=Integer.parseInt(answer[0]);
-                        year=Integer.parseInt(answer[1]);
-                        
-                        Page page =pageService.gatPageForm("sample-exit");
-                        page.getConditions.get(0).setVisibilityConfig(year<2018||(year==2018 && month<8));
-                    }
-                }
-            }
-        }).doneCondition().build();
+.isVisible(()->{
+			String answer[] = new String[2];
+			Date courseStartDate = null;
+			Date actualDate = null;
+			Map<String, Map<String, Object>> applicationAnswer = null;
+			Map<String, Object> isAnsweredPreviousQuestion = null;
+			SimpleDateFormat sdf= new SimpleDateFormat("MM/yyyy");
+
+			if (null != this.applicationAnswers.getAnsweres()) {
+				try {
+					applicationAnswer = applicationAnswers.getAnsweres();
+					isAnsweredPreviousQuestion = applicationAnswer.get("course-start-date-details");
+					if (null != isAnsweredPreviousQuestion) {
+						answer = (String[]) isAnsweredPreviousQuestion.get("course-start-details");
+					}
+
+					Page page =pageService.getPageForm("sample-exit");
+					if (null != applicationAnswer.get("course-started-date-details")) {
+						page.getConditions.get(0).setVisibilityConfig(true);
+					} else if (answer[0] != null && answer[1] != null) {
+						actualDate = sdf.parse("08/2018");
+						courseStartDate = sdf.parse(answer[0] + "/" + answer[1]);
+						page.getConditions.get(0).setVisibilityConfig(courseStartDate.before(actualDate));
+					}else {
+						page.getConditions.get(0).setVisibilityConfig(false);
+					}
+
+				} catch (Exception e) {
+					logger.error("Exception occured while getting course start details information" + e);
+				}
+			}
+		}).doneCondition().build();
+
+
+//Below one for if condition check
+
+if (page.getConditions().isEmpty()|| (conditionalQuestions.contails(page.getName()) && page.getConditions().get(0).isVisibilityConfig()) || (!page.getConditions().isEmpty() && page.getConditions().get(0).isVisibilityConfig())) {
+
+
+
